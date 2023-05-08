214d3334b4964b5e7aee329a98d2d83b497a4901
Louise Naginski <louisindiana@gmail.com>
2023-05-08 11:08:15 +0200

    create instances of business component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { Business } from './Components/Business';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ravenous</h1>
+      <br></br>
+      <Business />
+      <Business />
+      <Business />
+      <Business />
+      <Business />
+      <Business />
+
     </div>
   );
 }
